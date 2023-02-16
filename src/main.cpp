784601406252174ec287abfa95665177ccc55342
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-02-16 16:23:18 -0600

    added commands singleton for functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <intercept.hpp>
+#include "commands.hpp"
+#include "Geometry/rotator.hpp"
 
+// keep this global, so commands can't go out of scope.
+Commands& commands = Commands::get();
 
 int intercept::api_version() { //This is required for the plugin to work.
     return INTERCEPT_SDK_API_VERSION;
@@ -10,9 +14,9 @@ void intercept::register_interfaces() {
 }
 
 void intercept::pre_start() {
-    
+    Rotation::Rotator::pre_start();
 }
 
 void intercept::pre_init() {
-    intercept::sqf::system_chat("The Intercept template plugin is running!");
+    
 }
