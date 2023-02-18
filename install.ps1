c028546c4ed3d2b36cb25699963b36968672902d
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-02-18 16:54:33 -0600

    cmake now places files in the right place

--- install.ps1
+++ install.ps1
@@ -13,7 +13,10 @@ else
 	{
 		"installing files"
 		$copyPath = "$PSScriptRoot\build\win64\csc_x64.dll"
-		Copy-Item -Path $copyPath -Destination "$installPath\intercept\csc_x64.dll" -Force
+		Copy-Item -Path $copyPath -Destination "$installPath\Arma 3\@csc\intercept\csc_x64.dll" -Force
 	}
-
+	else
+	{
+		"path is bad"
+	}
 }
